9788fbeedbf222aaf610e131b726010ab2daa255
Ta-Tos <brotherhoodmusebiz@gmail.com>
2022-08-27 11:25:19 +0900

    ActiveHashを用いたセレクトボックスの作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,4 +4,11 @@ class ItemsController < ApplicationController
 
   def new
   end
+
+  def create
+  end
+
+
+  private
+  
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category, :delivery_charge, :shipping_day, :item_condition, :prefecture
 end
