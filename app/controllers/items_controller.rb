3fb2ea3a2a3cf9f25c66454ad6f3b30bbc59d5b4
Ta-Tos <brotherhoodmusebiz@gmail.com>
2022-08-27 13:55:47 +0900

    出品データの保存とバリデーションの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,14 +1,25 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
+
   def index
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
 
   private
-  
+  def item_params
+    params.require(:item).permit(:image, :item_name, :item_explain, :category_id, :delivery_charge_id, :item_condition_id, :shipping_day_id, :prefecture_id, :price).merge(user_id: current_user.id)
+  end
 end
