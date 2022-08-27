22b5936f1959abee47aa466fc57d8b7ea23d405d
Ta-Tos <brotherhoodmusebiz@gmail.com>
2022-08-27 22:17:39 +0900

    手数料、利益　カンマ区切りと小数点切り捨て追加

--- app/javascript/taxprice.js
+++ app/javascript/taxprice.js
@@ -2,9 +2,10 @@ function inputChange(){
   const itemprice = document.getElementById("item-price")
   const taxprice = document.getElementById("add-tax-price")
   const profitprice = document.getElementById("profit")
-  tax = itemprice.value * 0.1
-  taxprice.innerHTML = tax
-  profitprice.innerHTML = itemprice.value - tax
+  tax = Math.floor(itemprice.value * 0.1)
+  taxprice.innerHTML = tax.toLocaleString()
+  value = itemprice.value - tax
+  profitprice.innerHTML = value.toLocaleString()
 }
 
 window.addEventListener('input', inputChange)
